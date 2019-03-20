fadc712c97bf16d705fe2263ca58c97484e37b7a
bhold6160 <bhold6160@gmail.comgit config --listgit config --global user.email bhold6160@gmail.com>
2019-03-20 10:40:58 -0700

    added comment

--- python/two-fer/two_fer.py
+++ python/two-fer/two_fer.py
@@ -1,2 +1,6 @@
 def two_fer(name='you'):
+    """
+    setting the default param to "you" and using
+    .format to return the correct sentence
+    """
     return 'One for {}, one for me.'.format(name)
